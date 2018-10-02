c9c42872b03cf0ba966c37d4d6ead507d954f60b
Manoj Bisht <mbisht@MacBook-Pro-MJ.local>
2018-10-02 22:33:00 +0100

    Add: getStore

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,15 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import getStore from './getStore';
+import { Provider } from 'react-redux';
 
+const store = getStore();
 const render = (_App)  =>{
     ReactDOM.render (
+        <Provider store={store}>
         <_App />
-        ,document.getElementById('AppContainer'));
+        </Provider>,
+        document.getElementById('AppContainer'));
 };
 render(App);
