a0844b11059b48c3bf8ac03ca103396994d7902c
Manoj Bisht <mbisht@MacBook-Pro-MJ.local>
2018-10-04 10:23:48 +0100

    react router

---
+++ src/components/QuestionDetail.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Markdown from 'react-markdown';
+import TagsList from './TagsList';
+import { connect } from 'react-redux';
+
+// const QuestionDetailDisplay = ({title,body,answer_count,tags}) => (
+//     <div>
+//         <h3 className="mb-2">
+//             {title}
+//         </h3>
+//         {body ? <div>}
+//     </div>
+// )
