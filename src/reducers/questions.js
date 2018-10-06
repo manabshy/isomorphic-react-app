ebe0a5fc1090443d433d9169f0cb7382fbbdacac
Manoj Bisht <mbisht@MacBook-Pro-MJ.local>
2018-10-06 22:49:20 +0100

    ADD: Routes,Production settings

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,10 +1,30 @@
 import unionWith from 'lodash/unionWith';
 
-export const questions = (state = [], {type,questions}) => {
-    const questionEquality = (a = {}, b = {}) =>{
-        return a.question_id == b.question_id;
+/**
+ * Questions reducer, deals mostly with actions dispatched from sagas.
+ */
+export const questions = (state = [],{type,question,questions})=>{
+    /**
+     * Question Equality returns true if two questions are equal, based on a weak check of their question_id property
+     * @param a
+     * The first question
+     * @param b
+     * The second question
+     * @returns {boolean}
+     * Whether the questions are equal
+     */
+    const questionEquality = (a = {},b = {})=>{
+        return a.question_id == b.question_id
     };
-    if (type==="FETCHED_QUESTIONS") {
+
+    /**
+     * Create a new state by combining the existing state with the question(s) that has been newly fetched
+     */
+    if (type === `FETCHED_QUESTION`) {
+        state = unionWith([question],state,questionEquality);
+    }
+
+    if (type === `FETCHED_QUESTIONS`) {
         state = unionWith(state,questions,questionEquality);
     }
     return state;
